217cef825cc3a5cea4ca286803d94cb39dbf71f0
Jörg Schaede <j.schaede@gymnasium-oberstadt.de>
2017-03-21 15:10:19 +0100

    Added an enemey

---
+++ JumpNRun/Assets/Scripts/GroundDetector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundDetector : MonoBehaviour {
+
+	private bool onGround = true;
+
+	void OnColissionStart (Collider ground) {
+		onGround = true;
+
+	}
+	
+	// Update is called once per frame
+	void OnColissionExit (Collider ground) {
+		onGround = false;
+	}
+
+	public bool isOnGround(){
+		return onGround;
+	}
+}
