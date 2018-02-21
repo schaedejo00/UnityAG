927b17f0591e3f1d71ed31006cac0f2e273ff8a9
Lundralion <Lundralion@gmail.com>
2018-02-21 18:40:59 +0100

    Hab den TowerManager geschrieben der dafür sorgen sol, dass der Turm seine Textur wechselt.
    !!!ACHTUNG!!! Der TowerManager ist noch lange nicht ausgereift und enthält viele Fehler.

--- Bloobering/Assets/Scripts/TowerManager.cs
+++ Bloobering/Assets/Scripts/TowerManager.cs
@@ -5,24 +5,68 @@ using UnityEngine;
 public class TowerManager : MonoBehaviour {
 
 	public Texture textures;
+	public float cooldown;
+	public float exitTimerPerSecond;
 
 	private MeshRenderer meshRenderer;
 	private Renderer rend;
+	private bool playerStay;
+	private float timer;
+	private float pauseCooldown;
+	private float pauseCooldownTimer;
 
 	// Use this for initialization
 	void Start () {
-		
+		pauseCooldown = cooldown + 1;
 	}
-	
+	//timer - Time.time
 	// Update is called once per frame
 	void Update () {
-		
+		if (timer - Time.time > 0) {
+			
+			if (playerStay == false) {
+				Debug.Log (pauseCooldown);
+			} else {
+				Debug.Log (timer - Time.time);
+			}
+
+			if (cooldown >= pauseCooldown + exitTimerPerSecond) {
+				if (Time.time > pauseCooldownTimer) {
+					//Debug.Log ("Pause_Cooldown_wurde_erhöt");
+					pauseCooldown = pauseCooldown + exitTimerPerSecond;
+					pauseCooldownTimer = Time.time + 1;
+				}
+			} else {
+				pauseCooldown = cooldown;
+			}
+
+		}
 	}
+
 	void OnTriggerEnter (Collider col){
-		//Debug.Log ("Berührung_erkannt");
+		playerStay = true;
+		if (col.gameObject.tag == "Player") {
+			if (pauseCooldown < cooldown) {
+				timer = Time.time + pauseCooldown;
+			} else {
+				timer = Time.time + cooldown;
+			}
+		}
+	}
+
+	void OnTriggerStay (Collider col){
 		if (col.gameObject.tag == "Player"){
-			//Debug.Log ("PLayer_erkannt");
-			GetComponentInChildren<MeshRenderer>().material.mainTexture = textures;
+			if (Time.time >= timer) {
+				GetComponentInChildren<MeshRenderer> ().material.mainTexture = textures;
+			}
+		}
+	}
+
+	void OnTriggerExit (Collider col){
+		if (col.gameObject.tag == "Player") {
+			playerStay = false;
+			pauseCooldown = timer - Time.time;
+			pauseCooldownTimer = Time.time + 1;
 		}
 	}
 }
