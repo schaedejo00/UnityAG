f4962cb301bee9c367bd03231511e0a8d2a2fd07
Lundralion <Lundralion@gmail.com>
2018-02-24 21:14:31 +0100

    Der Turm kann nun zwiechen einem blauem und einem rotem Bloop unterscheiden.

---
+++ Bloobering/Assets/Scripts/TowerCountdown.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerCountdown : MonoBehaviour {
+
+public bool textureChanger = false;
+public float cooldown;
+public float exitTimerPerSecond;
+
+private Renderer rend;
+private bool playerStay = false;
+private float timer;
+private float pauseCooldown;
+private float pauseCooldownTimer;
+
+// Use this for initialization
+void Start () {
+	pauseCooldown = cooldown;
+}
+//timer - Time.time
+// Update is called once per frame
+void Update () {
+		if (GetComponent<TowerManager> ().firstPlayerChange == true) {
+			timer = Time.time + cooldown + (cooldown-(timer - Time.time));
+			GetComponent<TowerManager> ().firstPlayerChange = false;
+			Debug.Log ("Countdown_restarted");
+		}
+		if (timer - Time.time > 0) {
+			if (playerStay == false) {
+				Debug.Log (pauseCooldown);
+			} else {
+				Debug.Log (timer - Time.time);
+			}
+		}
+
+		if (playerStay == false) {
+			if (cooldown > pauseCooldown + exitTimerPerSecond) {
+				if (Time.time > pauseCooldownTimer) {
+					pauseCooldown = pauseCooldown + exitTimerPerSecond;
+					timer = Time.time + pauseCooldown;
+					pauseCooldownTimer = Time.time + 1;
+				}
+			} else {
+				pauseCooldown = cooldown;
+			}
+		}
+	}
+
+void OnTriggerEnter (Collider col){
+		if (col.gameObject.name == GetComponent<TowerManager>().firstPlayerStay) {
+		playerStay = true;
+		if (pauseCooldown < cooldown) {
+			timer = Time.time + pauseCooldown;
+		} else {
+			timer = Time.time + cooldown;
+		}
+	}
+}
+
+void OnTriggerStay (Collider col){
+		if (col.gameObject.tag == "Player") {
+			if (Time.time >= timer) {
+				textureChanger = true;
+			}
+		}
+	}
+
+void OnTriggerExit (Collider col){
+		if (col.gameObject.tag == GetComponent<TowerManager>().firstPlayerStay) {
+		playerStay = false;
+		pauseCooldown = timer - Time.time;
+		pauseCooldownTimer = Time.time;
+	}
+}
+}
--- Bloobering/Assets/Scripts/TowerManager.cs
+++ Bloobering/Assets/Scripts/TowerManager.cs
@@ -4,69 +4,56 @@ using UnityEngine;
 
 public class TowerManager : MonoBehaviour {
 
-	public Texture textures;
-	public float cooldown;
-	public float exitTimerPerSecond;
+	public Texture blueTexture;
+	public Texture redTexture;
+	public string firstPlayerStay = "false";
+	public bool firstPlayerChange = false;
 
-	private MeshRenderer meshRenderer;
-	private Renderer rend;
-	private bool playerStay;
-	private float timer;
-	private float pauseCooldown;
-	private float pauseCooldownTimer;
+	private string secondPlayerStay = "false";
+	private string newPlayer;
+	private string towerOwner = "false";
 
 	// Use this for initialization
 	void Start () {
-		pauseCooldown = cooldown;
+
 	}
-	//timer - Time.time
+
 	// Update is called once per frame
 	void Update () {
-		if (timer - Time.time > 0) {
-			if (playerStay == false) {
-				Debug.Log (pauseCooldown);
-			} else {
-				Debug.Log (timer - Time.time);
-			}
-		}
+		//GetComponent<TowerCountdown> ();
 
-		if (playerStay == false) {
-			if (cooldown > pauseCooldown + exitTimerPerSecond) {
-				if (Time.time > pauseCooldownTimer) {
-					pauseCooldown = pauseCooldown + exitTimerPerSecond;
-					timer = Time.time + pauseCooldown;
-					pauseCooldownTimer = Time.time + 1;
-				}
-			} else {
-				pauseCooldown = cooldown;
-			}
-		}
 	}
-
 	void OnTriggerEnter (Collider col){
 		if (col.gameObject.tag == "Player") {
-			playerStay = true;
-			if (pauseCooldown < cooldown) {
-				timer = Time.time + pauseCooldown;
-			} else {
-				timer = Time.time + cooldown;
+			if (towerOwner == "false") {
+				towerOwner = col.gameObject.name;
+			}
+			if (firstPlayerStay == "false") {
+				firstPlayerStay = col.gameObject.name;
+				firstPlayerChange = false;
 			}
 		}
 	}
-
 	void OnTriggerStay (Collider col){
-		if (col.gameObject.tag == "Player"){
-			if (Time.time >= timer) {
-				GetComponentInChildren<MeshRenderer> ().material.mainTexture = textures;
+		if (GetComponent<TowerCountdown>().textureChanger == true) {
+			if ("Sphere_blue" == firstPlayerStay) {
+				GetComponentInChildren<MeshRenderer> ().material.mainTexture = blueTexture;
+			}
+
+			if ("Sphere_red" == firstPlayerStay) {
+				GetComponentInChildren<MeshRenderer> ().material.mainTexture = redTexture;
 			}
 		}
 	}
-
 	void OnTriggerExit (Collider col){
-		if (col.gameObject.tag == "Player") {
-			playerStay = false;
-			pauseCooldown = timer - Time.time;
-			pauseCooldownTimer = Time.time;
+		if (secondPlayerStay == col.gameObject.name) {
+			secondPlayerStay = "false";
+		}
+		if (firstPlayerStay == col.gameObject.name) {
+			firstPlayerStay = secondPlayerStay;
+			firstPlayerChange = true;
+			secondPlayerStay = "false";
 		}
 	}
 }
+
